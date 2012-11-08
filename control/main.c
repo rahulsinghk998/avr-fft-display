e30f4a02ff32326dc03dcf7561a136dbf96a1c5f
wrmoy <william.r.moy@gmail.com>
2012-11-08 13:19:00 -0800

    added pseudocode

--- control/main.c
+++ control/main.c
@@ -26,3 +26,34 @@
  */
 
 /* PSEUDOCODE HERE */
+
+// system initialization, including:
+// -ports:
+//   -IO stuff
+//   -should probably do a quick test of IO...LED flash would be nice
+//   -set up TWI
+// -set up button interrupts
+// -set up debouncing timer
+// -defaults and variables
+
+// start control loop
+// read sample from ADC, depending on which input is selected
+// fill dat buffer
+// compute FFT
+// post-process output from code, i.e. frequencies
+// send result through TWI
+// end loop
+
+
+// other functions, may be here or eventually in other files:
+// timer stuff
+// button stuff:
+//   -input select, LED change and whatnot
+// reset stuff?
+// butt stuff
+// helper functions (probably going to be in common.c)
+
+
+/* Possible extra features */
+// sleep, maybe with another button, or repurpose reset button
+// ???
