809cc9c4445b98eea085a10ad3152e7ef8bc8496
wrmoy <william.r.moy@gmail.com>
2012-11-08 11:36:12 -0800

    added interrupt demo, including untested code.  NOT tested on hardware.

---
+++ demos/interrupt_demo/timer.h
@@ -0,0 +1,28 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+#include "types.h"
+
+extern int timerHits;
+
+enum TIMER_CLOCK_SELECT {
+	DISABLE_TIMER,
+	CLOCK_SCALE_1,
+	CLOCK_SCALE_8,
+	CLOCK_SCALE_64,
+	CLOCK_SCALE_256,
+	CLOCK_SCALE_1024,
+	EXT_CLOCK_FALLING,
+	EXT_CLOCK_RISING
+};
+
+void startTimer(int countTo, int clockSelect);
+void stopTimer(void);
+BYTE isTimerTripped(void);
+BYTE isTimerRunning(void);
+void clearTimerTripped(void);
+BYTE getTimerValue(void);
+void enableTimerInterrupt(void);
+void disableTimerInterrupt(void);
+
+#endif
