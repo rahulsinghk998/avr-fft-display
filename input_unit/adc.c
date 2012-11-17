185da18604035bb3f105e8d99a5949bd261a4910
William Moy <william.r.moy@gmail.com>
2012-11-16 20:29:39 -0600

    Added ADC interface and implementation.  Untested.

---
+++ input_unit/adc.c
@@ -0,0 +1,79 @@
+/*
+ *
+ *
+ *
+ */
+
+#include <avr/io.h>
+#include "adc.h"
+#include "common.h"
+
+void adc_init(void) {
+    bit_set(ADCSRA, ADEN);
+}
+
+void adc_set_prescaler(BYTE prescaler) {
+    ADCSRA &= 0xF8; // Zero out the biis 0-2
+    switch (prescaler) {
+        case ADC_CLK_DIV_2:
+            ADCSRA |= 1;
+            break;
+        case ADC_CLK_DIV_4:
+            ADCSRA |= 2;
+            break;
+        case ADC_CLK_DIV_8:
+            ADCSRA |= 3;
+            break;
+        case ADC_CLK_DIV_16:
+            ADCSRA |= 4;
+            break;
+        case ADC_CLK_DIV_32:
+            ADCSRA |= 5;
+            break;
+        case ADC_CLK_DIV_64:
+            ADCSRA |= 6;
+            break;
+        case ADC_CLK_DIV_128:
+            ADCSRA |= 7;
+            break;
+    }
+}
+
+void adc_select(BYTE inputOption) {
+    ADMUX &= 0xC0; // Zero out the bits 0-5
+    switch (inputOption) {
+        case ADC0_ADC3_20:
+            ADMUX |= 0x0B;
+            break;
+        case ADC1_ADC3_20:
+            ADMUX |= 0x0F;
+            break;
+        default:
+            ADMUX |= 0x20; // compare to GND
+            break;
+    }
+}
+
+void adc_enable_int(void) {
+    bit_set(ADCSRA, ADIE);
+}
+
+void adc_disable_int(void) {
+    bit_clear(ADCSRA, ADIE);
+}
+
+DWORD adc_get_value16(void) {
+    return (DWORD)ADCL & ((DWORD)ADCH << 8);
+}
+
+BYTE adc_get_value8(void) {
+    return (ADCL >> 2) & (ADCH << 6);
+}
+
+void adc_start(void) {
+    bit_set(ADCSRA, ADSC);
+}
+
+void adc_clear_flag(void) {
+    bit_set(ADCSRA, ADIF);
+}
--- input_unit/adc.h
+++ input_unit/adc.h
@@ -7,11 +7,91 @@
 #ifndef ADC_H
 #define ADC_H
 
+#include "types.h"
+
+enum ADC_PRESCALER_OPTIONS {
+    ADC_CLK_DIV_2,
+    ADC_CLK_DIV_4,
+    ADC_CLK_DIV_8,
+    ADC_CLK_DIV_16,
+    ADC_CLK_DIV_32,
+    ADC_CLK_DIV_64,
+    ADC_CLK_DIV_128
+};
+
+enum ADC_CHANNEL_OPTIONS {
+    ADC0,
+    ADC1,
+    ADC2,
+    ADC3,
+    ADC4,
+    ADC5,
+    ADC6,
+    ADC7,
+    ADC0_ADC0_1,
+    ADC0_ADC1_1,
+    ADC0_ADC3_1,
+    ADC1_ADC0_1,
+    ADC1_ADC2_1,
+    ADC1_ADC3_1,
+    ADC2_ADC1_1,
+    ADC2_ADC3_1,
+    ADC3_ADC0_1,
+    ADC3_ADC1_1,
+    ADC3_ADC2_1,
+    ADC3_ADC3_1,
+    ADC3_ADC4_1,
+    ADC3_ADC5_1,
+    ADC3_ADC6_1,
+    ADC3_ADC7_1,
+    ADC4_ADC3_1,
+    ADC4_ADC5_1,
+    ADC5_ADC3_1,
+    ADC5_ADC4_1,
+    ADC5_ADC6_1,
+    ADC6_ADC3_1,
+    ADC6_ADC5_1,
+    ADC6_ADC7_1,
+    ADC7_ADC3_1,
+    ADC7_ADC6_1,
+    ADC7_ADC7_1,
+    ADC0_ADC0_20,
+    ADC0_ADC1_20,
+    ADC0_ADC3_20,
+    ADC1_ADC0_20,
+    ADC1_ADC2_20,
+    ADC1_ADC3_20,
+    ADC2_ADC1_20,
+    ADC2_ADC3_20,
+    ADC3_ADC0_20,
+    ADC3_ADC1_20,
+    ADC3_ADC2_20,
+    ADC3_ADC3_20,
+    ADC3_ADC4_20,
+    ADC3_ADC5_20,
+    ADC3_ADC6_20,
+    ADC3_ADC7_20,
+    ADC4_ADC3_20,
+    ADC4_ADC5_20,
+    ADC5_ADC3_20,
+    ADC5_ADC4_20,
+    ADC5_ADC6_20,
+    ADC6_ADC3_20,
+    ADC6_ADC5_20,
+    ADC6_ADC7_20,
+    ADC7_ADC3_20,
+    ADC7_ADC6_20,
+    ADC7_ADC7_20
+};
+
 void adc_init(void);
+void adc_set_prescaler(BYTE);
 void adc_select(BYTE);
 void adc_enable_int(void);
 void adc_disable_int(void);
 DWORD adc_get_value16(void);
 BYTE adc_get_value8(void);
+void adc_start(void);
+void adc_clear_flag(void);
 
 #endif
