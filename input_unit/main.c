5096175be0f09503d6c760daca8433edf6258574
William Moy <william.r.moy@gmail.com>
2012-11-28 22:04:05 -0800

    Added left-align ACD result for speed, added x20 gain to ADC input.  Investigate if x20 is enough or if a HW amp is needed.

--- input_unit/main.c
+++ input_unit/main.c
@@ -88,8 +88,9 @@ int main(void) {
 
     /* Set up ADC */
     adc_init(); // initialize ADC hardware
+    adc_set_align(ADC_LEFT_ALIGN); // set the ADC value to be left-aligned
     adc_set_prescaler(ADC_CLK_DIV_8); // run ADC clock at 1MHz
-    adc_select(ADC0_ADC3_1); // read from ADC0, compare with ADC3, gain x1 for testing purposes
+    adc_select(ADC0_ADC3_20); // read from ADC0, compare with ADC3, gain x1 for testing purposes
     adc_enable_int(); // enable ADC interrupt on completed conversion
 
     /* Set up ADC capture timer */
@@ -120,14 +121,14 @@ ISR(TIM1_COMPA_vect) {
             ;
         // if reading from ADC 2, switch to ADC1
         if (bit_is_set(state, PB_STATE)) {
-            adc_select(ADC0_ADC3_1);
+            adc_select(ADC0_ADC3_20);
             bit_clear(state, PB_STATE);
             bit_set(PORTB, LED2_BIT);
             bit_clear(PORTB, LED1_BIT);
         }
         // else, switch to ADC 2
         else {
-            adc_select(ADC1_ADC3_1);
+            adc_select(ADC1_ADC3_20);
             bit_set(state, PB_STATE);
             bit_set(PORTB, LED1_BIT);
             bit_clear(PORTB, LED2_BIT);
