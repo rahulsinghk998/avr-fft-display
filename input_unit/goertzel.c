7b075f1a2d07d534fd1985ab96986f6e3623338a
William Moy <william.r.moy@gmail.com>
2012-12-25 07:23:25 -0500

    Generalized comments to apply to all frequencies

--- input_unit/goertzel.c
+++ input_unit/goertzel.c
@@ -46,21 +46,21 @@ static const BYTE coeff_mult[5][256] PROGMEM = \
 void goertzel_process_sample(BYTE sample8bit) {
     DWORD s = (DWORD)sample8bit;
     BYTE i;
-    // 16kHz
+    // sample frequency / 2
     q_0[0] = -q_1[0] - q_1[0] - q_2[0] + s;
-    // 8kHz
+    // F_s / 4
     q_0[1] = -q_2[1] + s;
-    // 4kHz
+    // F_s / 8
     q_0[2] = coeff_mult[0][q_1[2]]*2 - q_2[2] + s;
-    // 2kHz
+    // F_s / 16
     q_0[3] = coeff_mult[1][q_1[3]]*2 - q_2[3] + s;
-    // 1kHz
+    // F_s / 32 
     q_0[4] = coeff_mult[2][q_1[4]]*2 - q_2[4] + s;
-    // 500Hz
+    // F_s / 64
     q_0[5] = coeff_mult[3][q_1[5]]*2 - q_2[5] + s;
-    // 250Hz
+    // F_s / 128
     q_0[6] = coeff_mult[4][q_1[6]]*2 - q_2[6] + s;
-    // 125Hz
+    // F_s / 256 
     q_0[7] = q_1[7]*2 - q_2[7] + s;
     // Update older Q values
     for (i=0; i<8; i++) {
