8693c616498038375f89f3c996a1d608d1a69e45
William Moy <william.r.moy@gmail.com>
2012-12-28 18:14:23 -0500

    Modified sq16_to_bar for speed and code size, more code size changes on the way

--- input_unit/common.c
+++ input_unit/common.c
@@ -37,21 +37,26 @@ void delay_ms(DWORD ms) {
  *  BYTE: 8-bit display-friendly "bar"
  */
 BYTE sq16_to_bar8(DWORD a) {
+    BYTE val;
+    BYTE b = (BYTE)(a >> 8);
+    BYTE c = (BYTE)a;
     if (a == 0)
-        return 0x00;
-    if (0xC000 & a)
-        return 0xFF;
-    if (0x3000 & a)
-        return 0x7F;
-    if (0x0C00 & a)
-        return 0x3F;
-    if (0x0300 & a)
-        return 0x1F;
-    if (0x00C0 & a)
-        return 0x0F;
-    if (0x0030 & a)
-        return 0x07;
-    if (0x000C & a)
-        return 0x03;
-    return 0x01;
+        val = 0x00;
+    else if (0xC0 & b)
+        val = 0xFF;
+    else if (0x30 & b)
+        val = 0x7F;
+    else if (0x0C & b)
+        val = 0x3F;
+    else if (0x03 & b)
+        val = 0x1F;
+    else if (0xC0 & c)
+        val = 0x0F;
+    else if (0x30 & c)
+        val = 0x07;
+    else if (0x0C & c)
+        val = 0x03;
+    else
+        val = 0x01;
+    return val;
 }
