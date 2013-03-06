d0e042d059130683ba61c3b39680641904c67c23
wrmoy <william.r.moy@gmail.com>
2013-03-05 17:12:25 -0800

    Fixed the coeff_mult access from process_magnitudes

--- input_unit/goertzel.c
+++ input_unit/goertzel.c
@@ -173,28 +173,28 @@ void goertzel_process_magnitudes(DWORD* results) {
     sResults[0] = q_1[0]*q_1[0] - q_1[0]*q_2[0] + q_2[0]*q_2[0]; 
     sResults[1] = q_1[1]*q_1[1] + q_2[1]*q_2[1] + q_1[1]*q_2[1];
     sResults[2] = q_1[2]*q_1[2] + q_2[2]*q_2[2];
-    t = (q_2[3] > 0) ? 0x0000 & coeff_mult[0][(BYTE)(q_2[3])] : \
-                       0xFFFF & -coeff_mult[0][(BYTE)(-q_2[3])];
+    t = (q_2[3] > 0) ? 0x0000 & coeff_mult[(BYTE)(q_2[3])] : \
+                       0xFFFF & -coeff_mult(BYTE)(-q_2[3])];
     sResults[3] = q_1[3]*q_1[3] \
                  - q_1[3]*t\
                  + q_2[3]*q_2[3];
-    t = (q_2[4] > 0) ? 0x0000 & coeff_mult[1][(BYTE)(q_2[4])] : \
-                       0xFFFF & -coeff_mult[1][(BYTE)(-q_2[4])];
+    t = (q_2[4] > 0) ? 0x0000 & coeff_mult[(BYTE)(q_2[4])] : \
+                       0xFFFF & -coeff_mult[(BYTE)(-q_2[4])];
     sResults[4] = q_1[4]*q_1[4] \
                  - q_1[4]*t \
                  + q_2[4]*q_2[4];
-    t = (q_2[5] > 0) ? 0x0000 & coeff_mult[2][(BYTE)(q_2[5])] : \
-                       0xFFFF & -coeff_mult[2][(BYTE)(-q_2[5])];
+    t = (q_2[5] > 0) ? 0x0000 & coeff_mult[(BYTE)(q_2[5])] : \
+                       0xFFFF & -coeff_mult[(BYTE)(-q_2[5])];
     sResults[5] = q_1[5]*q_1[5] \
                  - q_1[5]*t \
                  + q_2[5]*q_2[5];
-    t = (q_2[6] > 0) ? 0x0000 & coeff_mult[3][(BYTE)(q_2[6])] : \
-                       0xFFFF & -coeff_mult[3][(BYTE)(-q_2[6])];
+    t = (q_2[6] > 0) ? 0x0000 & coeff_mult[(BYTE)(q_2[6])] : \
+                       0xFFFF & -coeff_mult[(BYTE)(-q_2[6])];
     sResults[6] = q_1[6]*q_1[6] \
                  - q_1[6]*t \
                  + q_2[6]*q_2[6];
-    t = (q_2[7] > 0) ? 0x0000 & coeff_mult[4][(BYTE)(q_2[7])] : \
-                       0xFFFF & -coeff_mult[4][(BYTE)(-q_2[7])];
+    t = (q_2[7] > 0) ? 0x0000 & coeff_mult[(BYTE)(q_2[7])] : \
+                       0xFFFF & -coeff_mult[(BYTE)(-q_2[7])];
     sResults[7] = q_1[7]*q_1[7] \
                  - q_1[7]*t \
                  + q_2[7]*q_2[7];
