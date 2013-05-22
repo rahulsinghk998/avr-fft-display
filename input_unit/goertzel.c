763bfc8bda00d5011082f502dc11797ea73559d8
William Moy <william.r.moy@gmail.com>
2013-05-22 13:27:58 -0400

    fixed bug: temporary calculation was accidentally zeroed out with an AND.  stupid, stupid mistake.

--- input_unit/goertzel.c
+++ input_unit/goertzel.c
@@ -66,7 +66,7 @@ void goertzel_process_sample(BYTE sample8bit) {
     for (i=0; i<8; i++) {
         // only update if shouldUpdate bit got switched on && not done sampling
         if ((shouldUpdate & freqMask) && (twiddleUpdated[i] < N_SAMPLES)) {
-            t = (q_1[i] >= 0) ? 0x0000 & coeff_mult[(BYTE)(q_1[i])] : \
+            t = (q_1[i] >= 0) ? 0x0000 | coeff_mult[(BYTE)(q_1[i])] : \
                                 -(0x0000 | coeff_mult[(BYTE)(-q_1[i])]);
             q_0[i] = t*2 - q_2[i] + s;
             twiddleUpdated[i]++;
